13af51086a62e9f194aaa8761e8c0f8214dd767a
cezar <cezar_h@yahoo.com>
2022-04-15 16:22:34 +0300

    final

--- src/main/java/ro/fasttrackit/curs22/homework/curs22homework/Curs22HomeworkApplication.java
+++ src/main/java/ro/fasttrackit/curs22/homework/curs22homework/Curs22HomeworkApplication.java
@@ -10,29 +10,4 @@ public class Curs22HomeworkApplication {
 		SpringApplication.run(Curs22HomeworkApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner atStartup(QuestionsRepository repo) {
-//		return args -> {
-//			repo.saveAll(List.of(
-//					new Questions(
-//							"Care este viteza maximă prevăzută de lege pentru circulaţia în zonele rezidenţiale?",
-//							"5 km/h;",
-//							"10 km/h;",
-//						"20 km/h;"),
-//					new Questions(
-//							"Este permisă oprirea voluntară a autovehiculului în tuneluri?",
-//							"da;",
-//							"nu;",
-//							"numai dacă lungimea tunelului depăşeşte 1000 m;"),
-//					new Questions(
-//							"Pe autostradă se interzice:",
-//							"circulaţia autovehiculelor în coloană;",
-//							"învăţarea conducerii unui vehicul, încercarea prototipurilor de şasiuri şi de vehicule cu motor;",
-//							"executarea de lucrări pe partea carosabilă pe timp de noapte;"),
-//					new Questions("În ce situaţie vă este permis să opriţi vehiculul sau să staţionaţi pe partea stângă a sensului de mers?",
-//							"pe drumurile cu sens unic, dacă rămâne liberă cel puţin o bandă de circulaţie;",
-//							"pe drumurile secundare;",
-//							"pe drumurile cu cel puţin două benzi pe sens;")));
-//		};
-//	}
 }
